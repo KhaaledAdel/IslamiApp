1cf472b4fdeca89851782daf22bfb45e5fde9684
Khaled <dolaadel9289@gmail.com>
2023-09-19 19:56:31 +0300

    initialize Islami App add Hadeth View add Radio View

--- lib/moduls/Radio/RadioView.dart
+++ lib/moduls/Radio/RadioView.dart
@@ -1,10 +1,41 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class RadioView extends StatelessWidget {
   const RadioView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var theme = Theme.of(context);
+    return Column(
+      children: [
+        Padding(padding: EdgeInsets.only(top: 150)),
+        Image.asset("assets/images/radio_image.png"),
+        SizedBox(height: 50),
+        Text("إذاعة القرآن الكريم"),
+        SizedBox(height: 50),
+        Row(
+          children: [
+            SizedBox(width: 50),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.arrow_circle_left,
+                  size: 65, color: theme.primaryColor),
+            ),
+            SizedBox(width: 60),
+            IconButton(
+              onPressed: () {},
+              icon:
+                  Icon(Icons.play_circle, size: 65, color: theme.primaryColor),
+            ),
+            SizedBox(width: 70),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.arrow_circle_right,
+                  size: 65, color: theme.primaryColor),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
